fdfc559e62875fd58ac4feccfbb932aa40c1eb87
saki.y <sakino.ym@gmail.com>
2022-04-17 09:39:48 +0000

    syuusei2

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,9 +2,9 @@ class Property < ApplicationRecord
   belongs_to :user
   
   validates :name, presence:true, length: { maximum: 50 }
-  validates :price, presence:true, length: { maximum: 50 }, numericality: true
+  validates :price, presence:true, length: { maximum: 50 }, numericality: { greater_than_or_equal_to: 0 }
   validates :address, presence:true, length: { maximum: 50 }
   validates :company, length: { maximum: 50 }
   
-  has_many :logs
+  has_many :logs, dependent: :destroy
 end
